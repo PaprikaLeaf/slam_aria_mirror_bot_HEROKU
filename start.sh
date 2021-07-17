bea210a295b09b97eb8288cc7c6154ab1a7f573a
PaprikaLeaf <87533542+PaprikaLeaf@users.noreply.github.com>
2021-07-17 10:45:47 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+tracker_list=curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat
+export tracker="[$tracker_list]"
 if [[ -n $TOKEN_PICKLE_URL ]]; then
 	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
 fi
