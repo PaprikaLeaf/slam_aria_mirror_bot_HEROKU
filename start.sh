5b55b9081fc3341cabe2c38efd37b90bef71ba22
PaprikaLeaf <87533542+PaprikaLeaf@users.noreply.github.com>
2021-07-17 15:54:57 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-tracker_list=curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n\n' ',')
 export tracker="[$tracker_list]"
 if [[ -n $TOKEN_PICKLE_URL ]]; then
 	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
